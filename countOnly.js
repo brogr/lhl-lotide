8531c5f10480cd6df2e944f181895deb4130fc77
Fabian Voegeli <dev@brogramming.com>
2021-11-14 20:30:54 +0000

    implemented countOnly

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// ASSERT FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// ACTUAL FUNCTION
+// countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+// The function should report back how many instances of each string were found in the allItems array of strings.
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      results[item] = (results[item]) ? results[item] + 1 : 1;
+    }
+  }
+
+  return results;
+};
+
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+console.log(result1);
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
