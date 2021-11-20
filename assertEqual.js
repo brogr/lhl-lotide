62003c22d3cf274f17986bb3158c9dad24d7a62e
Fabian Voegeli <dev@brogramming.com>
2021-11-20 18:28:18 +0000

    refactored assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
+// export
+module.exports = assertEqual;
